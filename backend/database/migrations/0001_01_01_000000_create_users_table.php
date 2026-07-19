7e748de465ccea67104b7750d487bb9568abd091
santiago sanchez chalarca <santiagoschalarca@gmail.com>
2026-07-18 19:23:59 -0500

    fix: remove duplicate role migration

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,7 +20,11 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('rol')->default('estudiante');
+            $table->enum('rol', [
+    'administrador',
+    'instructor',
+    'estudiante'
+])->default('estudiante');
             $table->rememberToken();
             $table->timestamps();
         });
